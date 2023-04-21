a7f5cb4641ae2a844b3bc2feda0fc58272a92a1f
raulalcaldecabo <raul.alcalde@inslapineda.cat>
2023-04-21 17:49:43 +0200

    user parte java

--- src/main/java/com/example/codeengine/expense/controller/UserController.java
+++ src/main/java/com/example/codeengine/expense/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.codeengine.expense.controller;
 
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +32,44 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+
+	public UserController(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/users")
+	Collection<User> users(){
+		return userRepository.findAll();
+	}
+		
+	//category/2
+	@GetMapping("/users/{id}")
+	ResponseEntity<?> getUser(@PathVariable Long id){
+	Optional<User> user = userRepository.findById(id);
+		return user.map(response -> ResponseEntity.ok().body(response))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+		
+	}
+			
+	@PostMapping("/users")
+	ResponseEntity<User> createCategory(@Valid @RequestBody User user) throws URISyntaxException{
+		User result= userRepository.save(user);
+		return ResponseEntity.created(new URI("/api/user" + result.getId())).body(result); 
+	
+	}	
+		
+	@PutMapping("/users/{id}")
+	ResponseEntity<User> updateUser(@Valid @RequestBody User user){
+		User result= userRepository.save(user);
+		return ResponseEntity.ok().body(result);
+	}
+			
+	@DeleteMapping("/users/{id}")
+	ResponseEntity<?> deleteUser(@PathVariable Long id){
+		userRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
 	
 	@PostMapping("/users/checkIfExists")
 	ResponseEntity<List<User>> findUser(@RequestBody User user) throws URISyntaxException{
